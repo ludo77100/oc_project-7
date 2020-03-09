1a8613325d258176784950f040982458b1e3fe19
ludo77100 <ludovic.s@protonmail.com>
2020-03-09 19:26:50 +0100

    Ajout de EmpruntController et des signature pour APIs emprunt dans MicroserviceLivresProxy

---
+++ clientui/src/main/java/org/ludo/clientui/controller/EmpruntController.java
@@ -0,0 +1,7 @@
+package org.ludo.clientui.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EmpruntController {
+}
--- clientui/src/main/java/org/ludo/clientui/proxies/MicroserviceLivresProxy.java
+++ clientui/src/main/java/org/ludo/clientui/proxies/MicroserviceLivresProxy.java
@@ -1,14 +1,20 @@
 package org.ludo.clientui.proxies;
 
+import org.ludo.clientui.beans.EmpruntBean;
 import org.ludo.clientui.beans.LivreBean;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @FeignClient(name = "bibliotheque", url = "localhost:8080")
 public interface MicroserviceLivresProxy {
+
+    /*
+    API Livre
+     */
 
     @GetMapping(value = "/liste")
     List<LivreBean> listeLivre();
@@ -16,4 +22,15 @@ public interface MicroserviceLivresProxy {
     @GetMapping(value = "/listeRecherche")
     List<LivreBean> listeLivreRecherche(@RequestParam(name = "mc")String mc);
 
+    /*
+    **************
+    API Emprunt
+    **************
+     */
+
+    @GetMapping(value = "/listeEmprunt")
+    List<EmpruntBean> listeEmprunt();
+
+    @GetMapping(value = "/emprunt/{idEmprunt}")
+    EmpruntBean findById(@RequestParam Long idEmprunt);
 }

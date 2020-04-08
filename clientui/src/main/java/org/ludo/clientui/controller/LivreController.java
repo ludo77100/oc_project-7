80e086ec7259e6b4ae9e71075525209199f6dea6
ludo77100 <ludovic.s@protonmail.com>
2020-04-08 16:05:28 +0200

    Ajout de la recherche par titre de livre sur le catalogue

--- clientui/src/main/java/org/ludo/clientui/controller/LivreController.java
+++ clientui/src/main/java/org/ludo/clientui/controller/LivreController.java
@@ -3,9 +3,12 @@ package org.ludo.clientui.controller;
 import org.ludo.clientui.beans.LivreBean;
 import org.ludo.clientui.proxies.MicroserviceBibliothequeProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,7 +23,18 @@ public class LivreController {
         List<LivreBean> listeLivres = livresProxy.listeLivre();
         model.addAttribute("listeLivresBean", listeLivres);
         System.out.println(listeLivres);
-        return "index";
+        return "/index";
+    }
+
+    @GetMapping(value = "/listeRecherche")
+    public String listeLivreRecherche(Model model, @RequestParam(name="mc")String mc){
+        System.out.println(mc);
+        List<LivreBean> listeLivres = livresProxy.listeLivreRecherche(mc);
+        System.out.println("2eme " + mc);
+        model.addAttribute("listeLivresBean", listeLivres);
+        model.addAttribute(mc);
+        System.out.println(listeLivres);
+        return "/index";
     }
 
 }

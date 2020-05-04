b4cfe5361ce46e10af9109a6994723f5651e6ce4
ludovic <ludovic.s@protonmail.com>
2020-05-04 09:31:57 +0200

    Ajout eureka pour les 3 ms

--- clientui/src/main/java/org/ludo/clientui/ClientuiApplication.java
+++ clientui/src/main/java/org/ludo/clientui/ClientuiApplication.java
@@ -2,11 +2,13 @@ package org.ludo.clientui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableFeignClients("org.ludo.clientui")
+@EnableEurekaServer
 public class ClientuiApplication {
 
 	public static void main(String[] args) {
--- utilisateur/src/main/java/org/ludo/Utilisateur/UtilisateurApplication.java
+++ utilisateur/src/main/java/org/ludo/Utilisateur/UtilisateurApplication.java
@@ -2,10 +2,12 @@ package org.ludo.Utilisateur;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaServer
 public class UtilisateurApplication {
 
 	public static void main(String[] args) {
